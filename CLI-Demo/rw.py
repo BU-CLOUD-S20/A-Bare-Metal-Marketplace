c444812f4e58b77d2b69e8a5fc0dd45f0705f00e
Parker Van Roy <parkervr@aol.com>
2020-02-12 15:58:26 -0500

    current db

--- CLI-Demo/rw.py
+++ CLI-Demo/rw.py
@@ -1,1 +1,8 @@
-ownership_77 = open('./db/ownership/77', 'r')
+import json
+
+with open('./db/organizations/orgs.json', 'w') as ownership_77:
+  o_77 = json.load(ownership_77)
+
+  print(o_77)
+
+#ownership_77.close()
